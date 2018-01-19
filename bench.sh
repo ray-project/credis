55bc873af041a6281ec62568a37f4f617b12abb6
Zongheng Yang <zongheng.y@gmail.com>
2018-01-19 15:52:29 -0800

    More script changes

--- bench.sh
+++ bench.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-for write_ratio in 1 0.66; do
+for write_ratio in 0.33 0.66 1; do
 
+
+    # Chain
+    for num_nodes in 1 2; do
+        echo 'num_clients throughput latency' > chain-${num_nodes}node-wr${write_ratio}.txt
+
+        for num_clients in $(seq 1 16); do
+            ./seqput.sh $num_clients $num_nodes $write_ratio
+            sleep 2
+        done
+
+
+    done
+
+    # Redis
     echo 'num_clients throughput latency' > redis-wr${write_ratio}.txt
-
     for num_clients in $(seq 1 16); do
-
         ./bench-redis.sh $num_clients $write_ratio
         sleep 2
-
     done
 
 
--- seqput.sh
+++ seqput.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -ex
+set -x
 NUM_CLIENTS=${1:-1}
 NUM_NODES=${2:-1}
-WRITE_RATIO=${3:1}
+WRITE_RATIO=${3:-1}
 
 # Example usage:
 #
@@ -13,7 +13,13 @@ WRITE_RATIO=${3:1}
 #   # To calculate throughput.
 #   grep throughput client<1-12>.log | cut -d' ' -f 11 | sort -n | tail -n1 | awk '{time=$1/1000} END {print 500000*12/time}'
 
+rm -rf client*.log
+pkill -f redis-server; ./setup.sh $NUM_NODES;
 for i in $(seq 1 $NUM_CLIENTS); do
     ./build/src/credis_seqput_bench $NUM_NODES $WRITE_RATIO 2>&1 | tee client$i.log &
 done
 wait
+
+thput=$(grep throughput client*.log | tr -s ' ' | cut -d' ' -f 11 | sort -n | tail -n1 | awk -v N=$NUM_CLIENTS '{time=$1/1000} END {print 500000*N/time}')
+latency=$(grep latency client*.log | tr -s ' ' | cut -d' ' -f 8 | awk '{s += $1} END {print s/NR}')
+echo "$NUM_CLIENTS $thput $latency" >> chain-${NUM_NODES}node-wr${WRITE_RATIO}.txt

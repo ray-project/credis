a4256307537064a99f6296a3248e3db0ef3196dd
Zongheng Yang <zongheng.y@gmail.com>
2018-01-19 15:47:40 -0800

    Add tr -s ' ' to trim whitespaces...

--- bench-redis.sh
+++ bench-redis.sh
@@ -11,6 +11,6 @@ for i in $(seq 1 $NUM_CLIENTS); do
 done
 wait
 
-thput=$(grep throughput client*.log | cut -d' ' -f 11 | sort -n | tail -n1 | awk -v N=$NUM_CLIENTS '{time=$1/1000} END {print 500000*N/time}')
-latency=$(grep latency client*.log | cut -d' ' -f 8 | awk '{s += $1} END {print s/NR}')
+thput=$(grep throughput client*.log | tr -s ' ' | cut -d' ' -f 11 | sort -n | tail -n1 | awk -v N=$NUM_CLIENTS '{time=$1/1000} END {print 500000*N/time}')
+latency=$(grep latency client*.log | tr -s ' ' | cut -d' ' -f 8 | awk '{s += $1} END {print s/NR}')
 echo "$NUM_CLIENTS $thput $latency" >> redis-wr${WRITE_RATIO}.txt

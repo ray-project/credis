6b00642b773ddd2cbc247a59da2e321bfa5436e4
Grayson Chao <grayson.chao@gmail.com>
2018-04-24 13:13:05 -0700

    Fix style issues in etcd utils

--- src/etcd/etcd_utils.cc
+++ src/etcd/etcd_utils.cc
@@ -4,7 +4,6 @@
 
 EtcdURL SplitEtcdURL(const std::string& url) {
   EtcdURL result;
-  result.url = url;
 
   auto start_of_chain_prefix = url.find_first_of('/');
   if (start_of_chain_prefix == std::string::npos) {
@@ -15,10 +14,5 @@ EtcdURL SplitEtcdURL(const std::string& url) {
   auto address = url.substr(0, start_of_chain_prefix);
   result.address = address;
 
-  auto start_of_port = address.find_first_of(':') + 1;
-  auto host = address.substr(0, start_of_port - 1);
-  auto port_str = address.substr(start_of_port);
-  result.host = host;
-  result.port = std::stoi(port_str);
   return result;
 }
--- src/etcd/etcd_utils.h
+++ src/etcd/etcd_utils.h
@@ -1,5 +1,5 @@
-#ifndef CREDIS_UTILS_H
-#define CREDIS_UTILS_H
+#ifndef CREDIS_ETCD_UTILS_H
+#define CREDIS_ETCD_UTILS_H
 
 #include <vector>
 #include "etcd3-cpp/include/etcd3.h"
@@ -7,17 +7,11 @@
 // Represents an etcd URL (e.g. "127.0.0.1:1234/prefix").
 struct EtcdURL {
  public:
-  // the full url string (127.0.0.1:1234/prefix)
-  std::string url;
   // 127.0.0.1:1234
   std::string address;
-  // 127.0.0.1
-  std::string host;
-  // 1234
-  int port;
   // "/prefix"
   std::string chain_prefix;
 };
 EtcdURL SplitEtcdURL(const std::string& url);
 
-#endif  // CREDIS_UTILS_H
+#endif  // CREDIS_ETCD_UTILS_H
